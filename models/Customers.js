69d1e493503fea8368c35cbbbcb65bb0c486039f
Kevin Le Franc <kevinlefranc@MacBook-Pro-de-Kevin.local>
2018-11-04 16:22:03 +0100

    indent customer

--- models/Customers.js
+++ models/Customers.js
@@ -18,16 +18,16 @@ customerSchema.pre('save', function(next) {
 
   // generate a salt
   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(customer.password, salt, function(err, hash) {
       if (err) return next(err);
 
-      // hash the password using our new salt
-      bcrypt.hash(customer.password, salt, function(err, hash) {
-          if (err) return next(err);
-
-          // override the cleartext password with the hashed one
-          customer.password = hash;
-          next();
-      });
+      // override the cleartext password with the hashed one
+      customer.password = hash;
+      next();
+    });
   });
 });
 
